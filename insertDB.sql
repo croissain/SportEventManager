db3b6bc714eee4b178f6620554f659ee4a83bd2a
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2021-12-24 01:34:03 +0700

    delete player

---
+++ insertDB.sql
@@ -0,0 +1,52 @@
+insert into "NhanVien"("MaNV","TenNV","SDT_NV","EmailNV","AnhNV","MatKhauNDK")
+values ('NV001','Trần Ngọc Sang','0123456789','sang1@gmail.com','#','$2b$05$1K8cnnvxyDmOf6/ZuaXJ/eK7TDYbbEbZZP35ugzNnTwmE6OBe9FAG');
+-- sang123
+-- $2b$05$1K8cnnvxyDmOf6/ZuaXJ/eK7TDYbbEbZZP35ugzNnTwmE6OBe9FAG
+
+insert into "NhanVien"("MaNV","TenNV","SDT_NV","EmailNV","AnhNV","MatKhauNDK")
+values ('NV002','Lê Thọ','012312415','tho1@gmail.com','#','$2b$05$ETRsQcnO7VsnKCaR/SsO6eaBouseuOFeweRh90Wween9ObQ7WFNBW');
+
+insert into "GiaiDau"("MaGD","TenGD","DoTuoiTGNhoNhat","DoTuoiTGLonNhat","SoDBThamGia","MaNV","HanCuoiDangKy")
+values ('GD001','AFF CUP','10','15','16','NV001','2021-12-26 0:0:0');
+
+insert into "GiaiDau"("MaGD","TenGD","DoTuoiTGNhoNhat","DoTuoiTGLonNhat","SoDBThamGia","MaNV","HanCuoiDangKy")
+values ('GD002','SeaGame','10','15','16','NV002','2021-12-24 0:0:0');
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK01','Nguyễn Thị Tiểu Mi','0987654321','mi1@gmail.com','#','$2b$05$uWU7XdaEPdRzz0aDBuajNOhXL4iegu7zj1H2BljrVXmpt6Fn1odgi');
+
+-- miabc123
+-- $2b$05$uWU7XdaEPdRzz0aDBuajNOhXL4iegu7zj1H2BljrVXmpt6Fn1odgi
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK02','super man','0113114115','super@gmail.com','#','$2b$05$0gfscB95b8kH3fZ8eDRU6uS5FWT/TEuHB918N9gM8br9CyWgYccIS');
+
+
+-- superxyz
+-- $2b$05$0gfscB95b8kH3fZ8eDRU6uS5FWT/TEuHB918N9gM8br9CyWgYccIS
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK03','Lê Thọ','012312415','tho1@gmail.com','#','$2b$05$ETRsQcnO7VsnKCaR/SsO6eaBouseuOFeweRh90Wween9ObQ7WFNBW');
+
+-- nhattho123
+-- $2b$05$ETRsQcnO7VsnKCaR/SsO6eaBouseuOFeweRh90Wween9ObQ7WFNBW
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK04','Huỳnh Tiến','0234235236','tien1@gmail.com','#','$2b$05$.QwWU/Ky2z.7OCmhoefaLOsWv1z3BD7XzgU26RegJFgNdcLB2dL9m');
+
+-- manhtienhjk
+-- $2b$05$.QwWU/Ky2z.7OCmhoefaLOsWv1z3BD7XzgU26RegJFgNdcLB2dL9m
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK05','Lê an','099988777','an1@gmail.com','#','$2b$05$O0SlEO11yTIK0/kPd1aO/etIBmmTkUCfL5JCQ0vrarGyVZZkcZlFy');
+-- sdjfdjsf
+-- $2b$05$O0SlEO11yTIK0/kPd1aO/etIBmmTkUCfL5JCQ0vrarGyVZZkcZlFy
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK06','Dương Tiêu','0333441411','tieu1@gmail.com','#','$2b$05$edAvkRX71Dbc8lsbHf4X1eDG7DNVYx0VmEYqPm.w3qnD0Cg7VBytK');
+
+-- 3r423424fdfdsf
+-- $2b$05$edAvkRX71Dbc8lsbHf4X1eDG7DNVYx0VmEYqPm.w3qnD0Cg7VBytK
+
+insert into "NguoiDangKy"("MaNDK","TenNDK","SDT","Email","AnhNDK","MatKhauNDK")
+values ('NDK07','Trương Vô Kỵ','0113222453','ky1@gmail.com','#','$2b$05$ugem5dhb1GxsolnJhd1FxeOIQI71SZV09l9X/vQC6L3D0lHxPwQTO');
